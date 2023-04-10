011e4e4024411b648c4c8af51916714a88b0291d
mi <mansurova.m94@gmail.com>
2023-04-10 18:45:53 +0300

    functions annotation (for VAF valculation)

--- calculate_VAF.py
+++ calculate_VAF.py
@@ -2,9 +2,39 @@ import vcf
 import numpy as np
 
 
-def get_vaf_dict(records: vcf.Reader) -> dict:
+def get_all_gt(records: vcf.Reader) -> list:
+    """
+    Receiving all genotype values from vcf.Reader records
+    :param records: vcf.Reader records
+    :return: list with all genotype values
+    """
+    all_gt_values = list()
+
+    for row in records:
+        samples = row.samples
+        for sample in samples:
+            gt_value = sample.data.GT
+            if gt_value not in all_gt_values:
+                all_gt_values.append(gt_value)
+
+    return all_gt_values
+
+
+def get_vaf_dict(records: vcf.Reader, correct_haploid_gt_values: set) -> dict:
+    """
+    Receiving cp_alleles_gt_vaf dictionary in the format below where
+    sample.data.AD1 / sum(sample.data.AD) is a VAF for each sample):
+        { chr_pos: alleles (for correct haploid genotype values):
+            gt_value: [sample.data.AD1 / sum(sample.data.AD), ..., sample.data.ADn / sum(sample.data.AD)]
+        }
+        example:
+        { 'Pf_M76611 33': 'T,A': '0/0': [[1.0, 0.0], ..., [1.0, 0.0]] }
+        { 'Pf_M76611 33': 'T,A': '0|1': [[0.9166666666666666, 0.08333333333333333]] }
+    :param records: vcf.Reader
+    :param correct_haploid_gt_values: set with the correct haploid genotype values
+    :return: cp_alleles_gt_vaf dictionary
+    """
     cp_alleles_gt_vaf = dict()
-    correct_haploid_gt_values = {'0/0', '0|0', '0/1', '0|1', '1/1', '1|1', './.', '0/.'}
 
     for row in records:
         chr_pos = f'{row.CHROM} {row.POS}'
@@ -33,16 +63,26 @@ def get_vaf_dict(records: vcf.Reader) -> dict:
     return cp_alleles_gt_vaf
 
 
-def print_output(cp_alleles: dict, outp_file: str) -> None:
+def write_output(cp_alleles: dict, outp_file: str) -> None:
+    """
+    Receiving mean VAF for each sample set for genotype and writing in the output file
+    :param cp_alleles: cp_alleles dictionary in the format below where
+    sample.data.AD1 / sum(sample.data.AD) is a VAF for each sample):
+        { chr_pos: alleles (for correct haploid genotype values):
+            gt_value: [sample.data.AD1 / sum(sample.data.AD), ..., sample.data.ADn / sum(sample.data.AD)]
+        }
+    :param outp_file: output filepath
+    :return: None
+    """
     outp_data = ''
     for chr_pos, alleles_gt_vaf in cp_alleles.items():
         for alleles, gt_vaf in alleles_gt_vaf.items():
             for gt, vaf in gt_vaf.items():
                 mx = np.matrix(vaf)
-                lst_sum = list(np.array(mx.sum(axis=0))[0])
-                lst_vaf = [x / mx.shape[0] for x in lst_sum]
+                sum_array = np.array(mx.sum(axis=0))[0]
+                vaf_lst = [x / mx.shape[0] for x in sum_array]
 
-                data_vaf = ','.join([str(x) for x in lst_vaf])
+                data_vaf = ','.join([str(x) for x in vaf_lst])
                 outp_data += f"{chr_pos} {alleles} {data_vaf}\n"
 
     with open(outp_file, 'w+') as f:
@@ -51,8 +91,12 @@ def print_output(cp_alleles: dict, outp_file: str) -> None:
 
 if __name__ == "__main__":
     vcf_path = './vcf_vaf_data/Pf_M76611.pf7.200_samples.vcf'
-    vcf_records = vcf.Reader(open(vcf_path, 'r'))
 
-    cp_alleles_dict = get_vaf_dict(records=vcf_records)
+    all_gt_values = get_all_gt(records=vcf.Reader(open(vcf_path, 'r')))
+    print(f"All genotype values list for the input records: {all_gt_values}")
 
-    print_output(cp_alleles=cp_alleles_dict, outp_file='./vcf_vaf_data/outp_vaf.txt')
+    cp_alleles_dict = get_vaf_dict(
+        records=vcf.Reader(open(vcf_path, 'r')),
+        correct_haploid_gt_values={'0/0', '0|0', '0/1', '0|1', '1/1', '1|1', './.', '0/.'}
+    )
+    write_output(cp_alleles=cp_alleles_dict, outp_file='./vcf_vaf_data/outp_vaf.txt')
